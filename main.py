bf93dcd515c39f7976463a9ab912dd1101f16bb6
sherry <sherry201901@126.com>
2025-12-17 20:37:26 +0800

    test

--- main.py
+++ main.py
@@ -10,9 +10,9 @@ def beifan():
 def test_case1(beifan):
     assert 1 + 1 == 2
 
-@pytest.mark.fixture
-def test_bbp(beifan):
-    assert 1+1 == 5
+# @pytest.mark.fixture
+# def test_bbp(beifan):
+#     assert 1+1 == 5
 
 if __name__ == '__main__':
     # print("hello world")
