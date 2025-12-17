34d34f48400bba35797a06ac882f28ec56bd5cbf
sherry <sherry201901@126.com>
2025-12-17 18:23:22 +0800

    First commit

---
+++ main.py
@@ -0,0 +1,19 @@
+import os
+import pytest
+@pytest.fixture()
+def beifan():
+    print("\nsetup")
+    yield
+    print("\nteardown")
+
+@pytest.mark.fixture
+def test_case1(beifan):
+    assert 1 + 1 == 2
+
+@pytest.mark.fixture
+def test_bbp(beifan):
+    assert 1+1 == 5
+
+if __name__ == '__main__':
+    # print("hello world")
+    test_case1(beifan())
