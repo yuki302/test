fe15065e2aba6948f1cfdfcaf93b2acd581b7702
sherry <sherry201901@126.com>
2025-12-17 22:25:10 +0800

    test

--- main.py
+++ main.py
@@ -1,18 +1,22 @@
 import os
 import pytest
+import allure
+
+@allure.title("setup")
 @pytest.fixture()
 def beifan():
     print("\nsetup")
     yield
     print("\nteardown123")
-
+@allure.title("success")
 @pytest.mark.fixture
 def test_case1(beifan):
     assert 1 + 1 == 2
 
-# @pytest.mark.fixture
-# def test_bbp(beifan):
-#     assert 1+1 == 5
+@allure.title("fail")
+@pytest.mark.fixture
+def test_bbp(beifan):
+     assert 1+1 == 5
 
 if __name__ == '__main__':
     # print("hello world")
